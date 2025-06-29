1c82203f31b401aab9f9e104c6531ca47e0f90cb
Ayush <bhupeshgamedev8777@gmail.com>
2025-06-29 22:51:21 +0530

    basic_movement_done

--- (fsm)-fps-controller/player/script/player.gd
+++ (fsm)-fps-controller/player/script/player.gd
@@ -5,18 +5,27 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
+#Node_Refrence
+@onready var head = %Head
+
+
+
+
+#variable
+@export var camera_senstivity : float = 0.02
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -26,3 +35,9 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+#------mouse_input----#
+func _input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_x(deg_to_rad(event.relative.y)*camera_senstivity)
+		rotate_y(deg_to_rad(event.relative.x)*camera_senstivity)
